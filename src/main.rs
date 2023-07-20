27595daa68201287480ae281b6f27af950ca87d0
Vijay J <81014244+yoyozaemon@users.noreply.github.com>
2023-07-20 09:29:55 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
-}
+use anyhow::{anyhow, Result};
+use clap::Parser;
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyModifiers},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use ignore::Walk;
+use rand::prelude::*;
+use std::io;
+use std::path::PathBuf;
+use std::time::{Duration, Instant};
+use tui::{backend::CrosstermBackend, Terminal};
+
+mod app;
+mod reader;
+mod types;
+mod views;
+use crate::views::{view, Theme};
+use app::App;
+use reader::file::FileReader;
+use types::typing::Typing;
+
